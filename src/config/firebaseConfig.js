fb50705bbdea5ccbd64b4d764b28e46d80e9c63b
DeepanshuSoni12 <86549313+DeepanshuSoni12@users.noreply.github.com>
2024-05-19 11:45:04 +0530

    Add files via upload

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA_M6wh2Qgt9h0zjr6DdsGcFKRb6Kr5zFc",
+    authDomain: "crypto-hunter-30509.firebaseapp.com",
+    projectId: "crypto-hunter-30509",
+    storageBucket: "crypto-hunter-30509.appspot.com",
+    messagingSenderId: "902386478334",
+    appId: "1:902386478334:web:8d97130fb7e3140d0209af"
+  };
+
+export default firebaseConfig;
